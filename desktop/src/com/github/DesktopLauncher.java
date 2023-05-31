7bb29c13f6bd22d9abab06e8d9f035c267f9ee9c
Leo Jiang <leojiang246@gmail.com>
2023-05-31 16:01:13 -0700

    basic edits

--- desktop/src/com/github/DesktopLauncher.java
+++ desktop/src/com/github/DesktopLauncher.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-
 	public static void main(String[] args){
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
