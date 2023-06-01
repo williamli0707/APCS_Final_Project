6830352d75a5420861a4af78fb6c2be8bcf7ccf9
Leo Jiang <leojiang246@gmail.com>
2023-05-31 23:04:23 -0700

    javadoc

--- test.java
+++ test.java
@@ -5,6 +5,13 @@ import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Test class
+ * @author William Li
+ * @version 6/7/23
+ * @author Period 5
+ * @author Sources - None
+ */
 public class test {
     public static void main(String[] args) throws IOException {
         System.out.println(login("abc", "abc"));
