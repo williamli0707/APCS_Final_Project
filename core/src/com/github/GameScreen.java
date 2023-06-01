18f4180faaee39f63ba06450424654fff229915c
William Li <williamhli0707@gmail.com>
2023-05-31 21:02:02 -0700

    fixed incorrect windows

--- core/src/com/github/GameScreen.java
+++ core/src/com/github/GameScreen.java
@@ -212,7 +212,7 @@ public class GameScreen implements Screen, InputProcessor {
 
         //3D
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT | (Gdx.graphics.getBufferFormat().coverageSampling?GL20.GL_COVERAGE_BUFFER_BIT_NV:0));
-        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth() * 2, Gdx.graphics.getHeight() * 2); // * 2 because ??
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); // * 2 because ??
 
         sceneManager.update(delta);
         sceneManager.render();
--- core/src/com/github/VictoryScreen.java
+++ core/src/com/github/VictoryScreen.java
@@ -2,6 +2,7 @@ package com.github;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -44,6 +45,7 @@ public class VictoryScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT | (Gdx.graphics.getBufferFormat().coverageSampling?GL20.GL_COVERAGE_BUFFER_BIT_NV:0));
         batch.begin();
         bg.draw(batch);
         batch.end();
